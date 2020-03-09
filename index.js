aed06bdcc1e6107b1fa7130dd29170a96a0552fa
Michael Stallings <stall84@gmail.com>
2020-03-09 18:39:22 -0400

    Have a working image-generating page (AJAX exercises part 1). Working on building the select list dropdown with breeds data from dog.ceo breeds endpoint

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 
 
+// as instructed created a function to fire when the button in the html is clicked
  function doggoClicker () {
 
         var button = document.getElementById('doggo-clicker');
         button.innerHTML = "Generating Doggo...";
 
-        axios.get('https://dog.ceo/api/breeds/image/random'); 
+        // using axios function/method to GET a random image from the dog.ceo API
+        axios.get('https://dog.ceo/api/breeds/image/random')
+        // using a 'promise' function (.then) to run after the api get request is made.. this will 
+        // use a callback function taking the parameter 'response' which I (think) is an axios specific keyword representing
+        // the payload of the GET request. This function will simply grab the html img-container, make a new div. add some styling
+        // and bootstrap classes to it, then append the actual image via template literal notation (${response.data.message})
+            .then(function(response) {
+
+                var imgContainer = document.getElementById('img-container');
+                var dogImgDiv = document.createElement('div');
+                var dogImg = document.createElement('img');
+                imgContainer.appendChild(dogImgDiv);
+                dogImgDiv.setAttribute('class', 'card m-2');
+                dogImgDiv.style = 'width: 16rem;';
+                dogImgDiv.appendChild(dogImg);
+                dogImg.setAttribute('src',`${response.data.message}`); 
+                button.innerHTML = "Generate Doggo"
+
+            }) ; 
         
 }
     
+// We've got a working page so far.
+// Here we're adding a new axios GET request to the dog.ceo breeds API endpoint.
+// This was done outside the above function in the global scope so that it runs and pulls the 
+// data down into the browser every time the page is refreshed. We will use this data later in our select list
+
+axios.get('https://dog.ceo/api/breeds/list')
+    .then(function(response) {
+        // console logging response param to confirm get request is working correctly
+        console.log(response);
+        
+    }
+    );
